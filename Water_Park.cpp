3ca6b8cc0031b149f6f442edaaf9589bf9aabf05
ashmit2c2 <codeatashmit@gmail.com>
2023-12-04 18:34:37 +0530

    codechef

---
+++ Water_Park.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solution(){
+    int w,h;
+    cin >> w >> h;
+    if(w>=60 && h<=130){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+int main(){
+    solution();
+}
