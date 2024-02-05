f92b08222f4a3a69b9a6eedc9c652ecce3fb05b6
ashmit2c2 <codeatashmit@gmail.com>
2024-02-05 15:18:22 +0530

    codechef

---
+++ difficulty-rating-order.cpp
@@ -0,0 +1,91 @@
+/*
+BB########&&&&#BGPYJ?7!!~~^^^^^:::^^^^^^^^^^^^^^^^~~~~~~~!!7777?????????7777?????????JJJJJJJJYYYYJJJ
+BB#########&&&#BG5YJ?7!!~~^^^^^:::::::::::::^^^^^^^^^^^^~~~!!!!77777777777777777777????JJJJJJJYYYJJJ
+BBB########&&&#BG5YJ?7!!~~^^^^:::::::::::::::::^^::::::^^^^~~~~!!!!!!7777777777777777???JJJJJJJYYYJJ
+BBB########&&##BP5YJ?7!~~^^^^::::::::::^^~!7??JYYJJJ??7!~~^^^^^~~~!!!!!777777777777777??JJJJJJJJJJJJ
+BBBB###########GP5J?7!!~^^^^::::::^!7?Y555555555555PPPPP55YJ?7!~~^^~~~~!!!!777777777777????JJJJJJJJ?
+BBBBBB########BG5YJ?7!~~^^::^~!?JY555555555555555555555PPPPPPP55YJ7!~~~~~!!!!!!!!!!!!!7777??????????
+BBBBBBB######BGP5YJ?7!~~~!7JY55555555555YYYYYJ???JY5YJJJJY555555PPP5YJ7~~~!!!!!!!!!!77777777????????
+GGBBBBBB#####BGP5YJ?77?Y555555555YYJJ??77!!?777??7??7?77????7JJJY555PPPY?!~!!!!7777777777777??7?????
+GGBBBBBBB####BGG5YJ?Y55555555555J777!7!777!77????J?J????????7????JJYPPPPP5J7!7777777777?????????????
+BBBBBBB#######BGPYJ55YYY555Y5555555YYYYY5YYYYYJYJY?YYJJJYYYY5555555PPPPPPGG5?777????????????????????
+BBBBBB########BBP5Y5YYYYYYYYYY55Y5555555555555555Y555YY555555555PPPP5PPPPGGGPJ7?????????????????????
+###########&&##BGYY5YYYYYYYYYYY555555Y5JY???J77Y?~?57?777?!!??JJ?Y55PPPPPPGGGP???JJ??????????????JJJ
+#######&&&&&&&&#PYY5YYYYYYYYYYY5YJJ?!~?77??JJYYYY5Y55YY5555YY555JJ?J?Y5PPPGGGG5?JJJJJJ??????JJJJJJYY
+###&&&&&&&&&&&&PYYY5YYYYYYYYYY5!7!77JJYY5555555555555555555555555555YJ??5PPGGGGYJJJJJJJJJJJJJJJJJYYY
+&&&&&&&&&&&&&&PYYYY5YYYYYYYYYY5JJ5Y55YYYYYYY5555YYYYYYY55555555555PPPPP5PPPPGGGPYJJJJJJJJJJJJJJJJJJY
+&&&&&&&&&&&&&GJYYYY5YYYYYYYYYYYYYYYYY5555YYJ???JJY555PGGBBBBBBBBBBBBBGGPPPPPGGGP5JJJJJJJJJJJJJJJJJJJ
+&&&&&&&&&&&&GJYYYYY5YYYYYYYYYYYYY55YJ?!~~~!!7J5GGBBB#&&&&@@@@@@@@@@@&&&##BBGGGGGPY????????????????JJ
+&&&&&&&&&&&#YYYYYYY5YYYYYYYYYY555Y!^::^!J5GGBB#&&@@@@@@@@@@@@@@@@@@@@@@@@@@&&#BBGP77777!77777777????
+&&&&&&&&&&&PYYYYYYYY5YYYYYY55P5J!!7J55GGB##&@@@@@@@@&&&&&&&&&&@@@@@@@@@@@@@@@@@&##B57~!!!!!!!!!!777?
+&&&&&&&&&&#YYY?77YYYYYYY55P5Y?!7Y5PGB##&@@@@@@@&&@&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@&&#5!^~~~~~~~~!!!7
+&&&&&&&&&&BYY7^7!Y5Y5PPP55YYJY5GGB#&&@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@&&B7^^^~~~~~~~~!
+&&&&&&&&&&PJ~~JYY55555YY55PPGBB#&@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@B~^^^^^^^^~~~
+&&&&&&&&&&5?!?YY5G&#########&&@@@@@@@@@@@@&&&&&&#&&###&#&&&##&####&&&&&&&&#B&@@@@@@BGGY!^^^^^^^^^^^~
+#######&&BYJYY5P#@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&####&##&####&&&&&&@@@@@&#B&@@@@@B^:::::::::^^^^^^^
+#########BYYYP#@@@@@@@@@@&@@@@&&@@@@@@@@@@@@@@&&&&&&&&&#&&&&&&&&&&@@@@@@@&##&@@@&#7::::::::::^^^^^^^
+##########5YP&@@@@&&##&&&&&@@@@@@@@@@@@&&@&&&&#G5P#&@@@&&@@@&BG##B&@@@@@@@@@@&&@&&#G~:::::::^^^^^^^^
+#########&GP@@@@@&&&BB&&#&&&@@@@@@@@@@@@@@@@&&@@##&&@@@&&@@@@##&@&@@@@@@@@@@@@@@@@@#~:^^^:^^^^^^^~~~
+##########GGGPB#&&&&&#BGPP#&&@@@@@@@@@@@@@@@&##&####&@@##&@@&&###&@&&@@@@&&&&&&@&@#7^^^^^^^^^^^~~~~!
+#########BPYP5PGP5B@@@#GPB#&&@@@@@@@@@@@@@@&########@@&###@@&############B#&&@@@@&?:^^^^^^^^^~~~~!!!
+##########B5B&PPY?YBB#&BG#&&&@@@@@@@@@@@&#BBBBBBBB#&@&B555B@@####BBBBBBBBBBB#&&&@B~^^^^^^~~~~!!!!777
+#########&&J7Y~!YGPYJJ5J7?JG#&@@@@@@@@@&&###BBBBBB#@&P!::!5&@&BBBBBBBBBBBBBBBBB#&J^^^^^~~~!!!777???J
+&&&&&&&&&&&B~~^!YPP?!?JJ7!~!P#&&@@&&&@@@@@&#BBBBBB&P7~~^:~JB&@####BBBBBBBBBBBBB&P~~~~~~!!!777??JJYYY
+&&&&&&&&&&&&?^^!7^^^~7YJ?7!!!JP#&&&&&&@@@&#BBBB##&5~~~~^:^7YG#&&####B##BBB#####5!~~~!!!7777???JJYY5P
+&&&&&&&&&&#&G!^^^^:^^75YJ77!!~!5&BGBBBBBBBBBBB###G7~~~~^:^!?5GB#&&&&&#####BB#BJ77777777?????JJJYY5PP
+&&&&&&&&&&###P!!~^:^~755J7!!7!~?B#BBBBBGGGGGGGGGG?~~~~~^:^~!?5P55B&###BBBGPP#PJJ??JJJJ?JJJJJYYYY55PG
+&&&&&&&##BBBBB57~~^~~!PGY7!!!!~~7YPGPPYJ?77!77?7!!?5GG5?7??YPGPGJ?5GBBBBGGGPB5JYJJJYYYYYYYYYY555PPGG
+&&&&&&#BBB####BP?~^^::?GY7!!!!~~~~~~^^^^:::^!!~:.!P#BBB#BB####BBGJ!?YPGGGGGPPJYYYYYYYYYYY5555555PPGG
+&&&&&&&&##&&#BPB&BY7~^!YJ?!!!!~~~~~~~^^^^^^!~^:::^!?777YB&&&&&&BBG?!7J5PGGGPP55555YYY55555555PPPGGGG
+&&&&&&&&###BPYPB#@@&#BGYJJ77!!!~~~~~~~~^^^!~^~~~!!!!7775PYP#&@&&##G?7?Y5PGGGP55PPPPP5555555PPPGGGGGP
+&&&&&&&##BBP5PGGGB#GG##PYJ??77!!~~!!~~~^~7!!7?777777!!?5577JPB##&&&G??Y5PPGGP55PPPPPPP555555555555YY
+&&&&&&&&#BGPGGGGGG#J!!!J5JJ7???7!!!!!~~!????JJ??????7???77J55PGBB#&&G?YPPGPP5YY55YYYYYYYYYYYJJJ?????
+&&&&&&&&##B##BBGBB#J!~~!YYJ??JYJ7777!~!?J?5GBGPPP5555555PGBBBBB#####&5JPGBBGYYYYYYJJJJJJJ???77!!!!!!
+&&&&&&&&&#&&&##BBBBJ!!~~75YYY?YYJ?JYJ7!!?5GPJ7!!~~~~~~~~!77??J5GB#&##PJPBBBPJJJJJJ??????777!!~~~~~~~
+&&&&&&&&&&&&&&&#BBBJ!!!!!7PPP5YJJYY55J????7^^^!!7JJY5Y55Y555PPGPPPGGPJYB#&BJ???????77777!!!~~~~^^^^^
+&&#&&&&&&&&&&&&##BBJ!!!~~!!5BBP5YYJYPGP5PJ!~^~!!7JJ5PB####B###BGPP555PB##BJ??????777!!!!~~~~~~^^^^^^
+########&&&&&&&&###J7!!~~~~~J##B5YJJYPGPGY777~~7!77?Y5P###BBBGGGBGGGB#B#GJ77????77!!!!!~~~~^^^^^^^^^
+###########&&&&&&&#?7!!~~~^~~?B&#G5YJ?JY5YJY?7!^^^^~!!7JJYYYYY55PBB##BBBY77?????77!!!~~~~^^^^^^::::^
+##############&&&&B77!!!~~~^~~7G&#BBB5Y5P5JYYJ?7~!?77!77?JJY5PP5GB#&##BGJ7??????777!!~~~^^^^^^^:::^^
+##############&&&&J!77!!!!~~~~^~5##B###BGGG55YYYJJYJJY?JJJY5PBB###&&#BBGYJJJJJJ????7!!~~^^^^^^^^^^^^
+##############&&&P~!!7!!~~~~~~^^^7P####&###BGPPG55P5PP5PPPGBB##&&&&#BBBGGPYYYJJJJJ??7!!~~^^^^^^^^^^^
+B##########&###P7^^~!!!!~~~~~~~^^^~JG##&&##&&&&##B######&&&&&&&&&#BBBBGGGBBGGG5YJJ??77!!~~~~~~^~~~~~
+BBBB#####&#GPJ~^^^^~!!!!~~~~~~^^^^^^!JPB#&&&&&&&&&&&&&&&&&&&&&&#BBBBBGGGGBBBBB&#BG5J777!!!~~~~~~~!!!
+BBBB###BGP5J^::^^^^~!!!!~~~~~~~^^~~~~~!?5B###&&&&&&&&&&######BBBBBBBBGGGBBBBBB&&&&&#GYJ7!!!!!!!!777!
+GGBBBGP5JJYY!:::^^^^~!!~~~~~~~~~^^^~~!!!7?PB##########BBBBBBBBBBBBBBGGBBBBBBB#&&&&&&&&#BPY?7!!777777
+GGPP5555YYY55?~::^^^^!!~~~~~~~~~~~~~~~!777?JPBBBBBBBBBBBBBBBBBBBBBBGGGGBBBBB#&&&&&&&&&&&&&&BG5YJ????
+55555555YYYY555?~^:^^~~~~~~~~~~~~^^^^^^^~!!7?5GBBBBBBBBBBBBBBBBBBBGGGGGGBBB#&&&&&&&&&&&&&&&&&&&#BGP5
+555555YYJJYY5Y555J!^^~~~^^^~~~~~~^^^^^^~~~~~!!75GBBBBBBBBBBBBGGBBGGGGGGGB#&&&&&&&&&&&&&&&&&&&&&&&&&&
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void solution(int test){
+    while(test--){
+        int n;
+        cin >> n;
+        vector<int> v;
+        for(int i = 0; i<n ; i++){
+            int x;
+            cin >> x;
+            v.push_back(x);
+        }
+        int count = 0 ;
+        for(int i = 1; i<n ; i++){
+            if(v[i]<v[i-1]){
+                count++;
+            }
+        }
+        if(count>0){
+            cout << "No" << "\n";
+        }
+        else{
+            cout << "Yes" << "\n";
+        }
+
+    }
+}
+int main(){
+    int test;
+    cin >> test;
+    solution(test);
+}
