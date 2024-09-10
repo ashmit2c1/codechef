bc8828945f7edb266b98ed84656da19064da50f0
Ashmit <147606895+ashmit2c1@users.noreply.github.com>
2024-09-10 22:02:21 +0530

    Add files via upload

---
+++ Card_Removal.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long
+#define forloop(x,y) for(int i=x;i<y;i++)
+#define secondfor(x,y) for(int j=x;j<y;j++)
+#define print(x) cout << x << "\n";
+#define output(arr) for(int i=0;i<arr.size();i++){cout<<arr[i]<<" ";}
+#define asort(v) sort(v.begin(), v.end())
+#define dsort(v) sort(v.begin(), v.end(), greater<int>())
+#define intmax INT_MAX
+#define intmin INT_MIN
+#define lintmax LLONG_MAX
+#define lintmin LLONG_MIN
+#define mp(x,y) make_pair(x,y)
+
+void solution(int test){
+    while(test--){
+        lint n;
+        cin >> n;
+        vector<lint>arr;
+        forloop(0,n){
+            lint x; cin >> x;arr.push_back(x);
+        }
+        unordered_map<lint,lint>mp;
+        forloop(0,n){
+            mp[arr[i]]++;
+        }
+        lint maxf=LLONG_MIN;
+        for(auto it=mp.begin();it!=mp.end();it++){
+            lint freq=it->second;
+            maxf=max(maxf,freq);
+        }
+        print(n-maxf)
+    }
+}
+int main(){
+    int test;
+    cin >> test;
+    solution(test);
+}
---
+++ Even_tual_Reduction.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long
+#define forloop(x,y) for(int i=x;i<y;i++)
+#define secondfor(x,y) for(int j=x;j<y;j++)
+#define print(x) cout << x << "\n";
+#define output(arr) for(int i=0;i<arr.size();i++){cout<<arr[i]<<" ";}
+#define asort(v) sort(v.begin(), v.end())
+#define dsort(v) sort(v.begin(), v.end(), greater<int>())
+#define intmax INT_MAX
+#define intmin INT_MIN
+#define lintmax LLONG_MAX
+#define lintmin LLONG_MIN
+#define mp(x,y) make_pair(x,y)
+
+bool checkFunction(string s){
+    unordered_map<char,lint>mp;
+    forloop(0,s.size()){
+        char a=s[i];
+        mp[a]++;
+    }
+    for(auto it=mp.begin();it!=mp.end();it++){
+        lint freq=it->second;
+        if(freq%2==1){return false;}
+    }
+    return true;
+}
+
+void solution(int test){
+    while(test--){
+        lint n;cin >> n;
+        string s;cin >> s;
+        bool ans=checkFunction(s);
+        if(ans==true){print("YES")}
+        else{print("NO")}
+
+    }
+}
+int main(){
+    int test;
+    cin >> test;
+    solution(test);
+}
